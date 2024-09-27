7d2743f3d8c05f049b7771b98231b2d17fbbd5ad
Bosif <boannarutjes@gmail.com>
2024-09-27 19:36:04 +0200

    changed api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,13 +8,14 @@ export default function Dictionary() {
 
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        console.log(response.data);
     }
 
     function search(event){
         event.preventDefault();
-        
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+        //documentation: https://www.shecodes.io/learn/apis/dictionary
+        let apiKey = "c8a5e25887f10t1o4fc6a4fb7b8c3bcf";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
         axios.get(apiUrl).then(handleResponse);
     }
 
