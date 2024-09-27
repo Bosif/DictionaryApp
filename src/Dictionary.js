8deac4f3f85e3651e747d39568045bd5c6b72dff
Bosif <boannarutjes@gmail.com>
2024-09-27 20:05:14 +0200

    error fix

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import "./Dictionary.css"
 import axios from "axios";
+import Results from "./Results";
  
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState(null);
+    let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null)
 
 
     function handleResponse(response) {
-        console.log(response.data);
+        setResults(response.data);
     }
 
     function search(event){
@@ -28,6 +30,7 @@ export default function Dictionary() {
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange}/>
             </form>
+            <Results results={results}/>
         </div>
     )
 }
