3f6893f159480703cfaee5430b3d479d20be24f3
Bosif <boannarutjes@gmail.com>
2024-10-01 00:00:11 +0200

    fixed search engine

--- src/Results.js
+++ src/Results.js
@@ -7,8 +7,11 @@ export default function Results(props) {
     return (
         <div className="Results">
             <h2>{props.results.word}</h2>
-            {props.results.meanings.map(function(meanings, index) {
-                return meanings[0].definition;
+            {props.results.meanings.map(function(meaning, index) {
+                return <p>
+                    {meaning.definition}
+                    <br />
+                </p>;
             })}
         </div>
     );
